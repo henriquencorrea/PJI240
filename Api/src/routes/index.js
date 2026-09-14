b56746dd0d2155058f6e1e84daee0a395b1a16cd
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2026-09-14 19:47:42 -0300

    criado sessions.routes

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const usersRouter = require("./users.routes");
 const notesRouter = require("./notes.routes");
 const tagsRouter = require("./tags.routes");
-const sessionsRouter = require("./sessions.routes");
+const sessionsRouter = require("./sessions.routes.js");
 
 const routes = Router();
 routes.use("/users", usersRouter);
