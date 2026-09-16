d8190955f98df42ae8ec0bf3a92a6675e67e97de
Carlos Henrique Nunes Correa <carloscorrea.dpss@gmail.com>
2026-09-16 19:15:09 -0300

    criação de tema e estilo global e componente Heade

---
+++ Frontend/src/components/Header/index.jsx
@@ -0,0 +1,45 @@
+import { useAuth } from '../../hooks/auth'
+import { useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
+
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles";
+
+
+
+import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
+
+export function Header() {
+  const { signOut, user } = useAuth();
+  const navigation = useNavigate();
+
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+
+
+  }
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+
+    return(
+  
+    <Container>
+      <Profile to="/profile">
+        <img src={avatarUrl} alt={user.name}/>
+
+        <div>
+          <span>Bem Vindo</span>
+          <strong>{user.name}</strong>
+        </div>
+      </Profile>
+
+        <Logout onClick={handleSignOut}>
+          <RiShutDownLine />
+        </Logout>
+
+    </Container>
+    
+  );
+}
---
+++ Frontend/src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#3E3B47",
+
+    WHITE: "#F4EDE8",
+    ORANGE: "#FA7F08",
+            
+    GREEN: "#03A64A",
+    LIGHTGREEN: "#04BF8A",
+    
+    GRAY_100: "#999591",
+    GRAY_300: "#666360",
+
+    RED: "#FF002E",
+  }
+};
